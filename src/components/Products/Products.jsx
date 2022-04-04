7507d1ea0e2de7d1bbc50da7f8d0023e67a32fee
unknown <b160086@nitsikkim.ac.in>
2022-04-05 00:36:28 +0530

    added Navbar component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-
+import useStyles from "./Styles";
 import Product from "./Product/Product";
 
 const Products = () => {
@@ -22,8 +22,12 @@ const Products = () => {
         "https://www.cnet.com/a/img/At7JXOb2erGg-eOdKylQhFKfeJY=/2021/10/23/80425069-0d3e-4c67-9085-a66e6177fc60/macbook-pro-2021-cnet-review-12.jpg",
     },
   ];
+
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
